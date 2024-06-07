f3180dace0985072d2158c10390b67e1d4130477
smhrd1 <smhrd1@smhrd>
2024-06-07 10:41:45 +0900

    commit/Qr/test

--- maven/src/main/java/com/smhrd/controller/NameCardCon.java
+++ maven/src/main/java/com/smhrd/controller/NameCardCon.java
@@ -1,9 +1,9 @@
 package com.smhrd.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.servlet.RequestDispatcher;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,9 +11,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.smhrd.model.FarmhouseDAO;
 import com.smhrd.model.FarmhouseDTO;
 import com.smhrd.model.MemberDTO;
+
+import java.util.ArrayList;
 
 @WebServlet("/S_NameCardCon")
 public class NameCardCon extends HttpServlet {
@@ -27,18 +34,47 @@ public class NameCardCon extends HttpServlet {
         HttpSession session = request.getSession();
         MemberDTO user_info = (MemberDTO) session.getAttribute("user_info");
         System.out.println(user_info.getMb_id());
-        
+
         if (user_info == null) {
-        	moveURL = "Main.jsp";
-            System.out.println("명함발금안됨");
-        	
-          
-        } 
-        else {request.setAttribute("mb_id",user_info.getMb_id());
-        moveURL = "namecard.jsp";
-        System.out.println("명함발급..!");}
-        
-        RequestDispatcher rd = request.getRequestDispatcher(moveURL);
-		rd.forward(request, response);
+            moveURL = "Main.jsp";
+            System.out.println("명함발급안됨");
+            response.sendRedirect(moveURL);
+        } else {
+            String mb_id = user_info.getMb_id();
+            FarmhouseDAO dao = new FarmhouseDAO();
+            ArrayList<FarmhouseDTO> fh_dto = dao.getFarmhouseDTO(mb_id);
+
+            if (fh_dto.size() > 0) {
+                String fh_name = fh_dto.get(0).getFh_name();
+                String url = "http://192.168.0.25:8081/namecard.jsp?mb_id=" + mb_id + "&fh_name=" + fh_name;
+
+                ServletContext context = getServletContext();
+                String realPath = context.getRealPath("/images/qrcode.png");
+
+                try {
+                    generateQRCodeImage(url, 300, 300, realPath);
+                    request.setAttribute("qrCodePath", "images/qrcode.png");
+                    request.setAttribute("mb_id", mb_id);
+                    request.setAttribute("fh_name", fh_name);
+                    moveURL = "namecard.jsp";
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                    moveURL = "Main.jsp";
+                }
+            } else {
+                moveURL = "Main.jsp";
+                System.out.println("농가 정보가 없습니다.");
+            }
+            request.getRequestDispatcher(moveURL).forward(request, response);
+        }
+    }
+
+    private void generateQRCodeImage(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
     }
 }
--- maven/src/main/webapp/namecard.jsp
+++ maven/src/main/webapp/namecard.jsp
@@ -13,31 +13,38 @@
 <body>
     <%
         String mb_id = (String) request.getAttribute("mb_id");
+        String fh_name = (String) request.getAttribute("fh_name");
+        String qrCodePath = (String) request.getAttribute("qrCodePath");
+        
         System.out.println("Received mb_id: " + mb_id);  // 디버깅용 출력
+        System.out.println("Received fh_name: " + fh_name);  // 디버깅용 출력
+        System.out.println("QR Code Path: " + qrCodePath);  // 디버깅용 출력
+
         FarmhouseDAO dao = new FarmhouseDAO();
         ArrayList<FarmhouseDTO> fh_dto = dao.getFarmhouseDTO(mb_id);
         System.out.println("Fetched FarmhouseDTO size: " + fh_dto.size());  // 디버깅용 출력
 
         // 이전 값을 저장할 변수
-        String prevMbId = "";
         String prevFhName = "";
         String prevFhOwner = "";
         String prevAgriName = "";
     %>
     <h2>농가 정보</h2>
     <% for(FarmhouseDTO x : fh_dto) { %>
-        
         <p>농가 이름: <%= x.getFh_name().equals(prevFhName) ? "" : x.getFh_name() %></p>
         <p>농장주: <%= x.getFh_owner().equals(prevFhOwner) ? "" : x.getFh_owner() %></p>
         <p>품명: <%= x.getAgri_name().equals(prevAgriName) ? "" : x.getAgri_name() %></p>
         <hr/>
         <%
             // 현재 값을 이전 값 변수에 저장
-            
             prevFhName = x.getFh_name();
             prevFhOwner = x.getFh_owner();
             prevAgriName = x.getAgri_name();
         %>
+    <% } %>
+    <% if (qrCodePath != null) { %>
+        <h3>QR 코드:</h3>
+        <img src="<%= qrCodePath %>" alt="QR Code">
     <% } %>
 </body>
 </html>
